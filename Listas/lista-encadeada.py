5b470b7afd6e382e19e465f9efbe9001564c82a8
andrey-pereira02 <andreypereiradev@gmail.com>
2026-09-08 14:02:55 -0300

    mais metodos de inserção e remoção em lista encadeada

--- Listas/lista-encadeada.py
+++ Listas/lista-encadeada.py
@@ -7,12 +7,55 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
+        self.length = 0
 
-    def insere_valor(self, valor):
+    def insere_valor_inicio(self, valor):
         novo = Node(valor)
 
         novo.proximo = self.head
         self.head = novo
+        self.length += 1
+
+    def insere_valor_fim(self, valor):
+        pass
+
+    def insere_valor_meio(self, pos, valor):  # pos é a posição
+        if pos > self.length or pos < 0:
+            return None
+        if pos == 0:
+            self.insere_valor_inicio(valor)
+        if pos > self.length:
+            self.insere_valor_fim(valor)
+        else:
+            novo = Node(valor)
+            current = self.head
+            count = 1
+            while count < pos:
+                current = current.proximo
+                count += 1
+            novo.proximo = current.proximo
+            current.proximo = novo
+            self.length += 1
+
+    def remove_inicio(self):
+        if self.length != 0:
+            self.head = self.head.proximo
+            self.length -= 1
+
+    def remove_meio(self, pos):
+        if pos > self.length or pos < 0:
+            return None
+
+        currentNode = self.head
+        previusNode = self.head
+        count = 0
+        while currentNode.proximo is not None and count < pos:
+            if count == pos:
+                previusNode.proximo = currentNode.proximo
+                self.length -= 1
+            previusNode = currentNode
+            currentNode = currentNode.proximo
+            count += 1
 
     def imprimir(self):
         atual = self.head
@@ -51,9 +94,9 @@ class LinkedList:
 
 
 n = LinkedList()
-n.insere_valor(10)
-n.insere_valor(20)
-n.insere_valor(30)
+n.insere_valor_inicio(10)
+n.insere_valor_inicio(20)
+n.insere_valor_inicio(30)
 
 n.imprimir()
 n.media()
